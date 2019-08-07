db42b31ad6bd90793c0d44cf3a5fe900a0011c5c
张明朝 <zhangmingchao9@jd.com>
2019-08-07 17:30:00 +0800

    增加query查询

--- routers/proxy.js
+++ routers/proxy.js
@@ -2,17 +2,21 @@ const router = require('koa-router')();
 const axios = require ('axios');
 const config = require('../config');
 const log4js = require('log4js');
+const url = require('url');
 
 const logger = log4js.getLogger();
 axios.defaults.headers.Cookie = config.cookie;
 
 router.post('/proxy*', async(ctx,next)=>{
     const tarHost =  ctx.params[0];
+    const search = url.parse(ctx.request.url).search;
     let params = ctx.request.body;  
-    logger.info("params为：", params);
-    const data = await axios.post(`${config.host}${tarHost}`,params);
-    logger.info("获取的数据为：", data.data);
-    logger.debug("Time:", new Date());
+    console.log("params为：", params);
+    console.log("请求地址为",`${config.host}${tarHost}${search}`)
+    const data = await axios.post(`${config.host}${tarHost}${search}`,params);
+    console.log("获取的数据为：", data.data);
+    console.log("Time:", new Date());
+
     ctx.body = data.data;
 })
 module.exports = router; 
