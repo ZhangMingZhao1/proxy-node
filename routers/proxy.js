d371c5a07901d170f90a46c11a042fc9ec4f92fc
张明朝 <zhangmingchao9@jd.com>
2019-09-10 18:05:23 +0800

    feat:15期支持

--- routers/proxy.js
+++ routers/proxy.js
@@ -12,12 +12,17 @@ router.post('/proxy*', async(ctx,next)=>{
     const tarHost =  ctx.params[0];
     const search = url.parse(ctx.request.url).search;
     let params = ctx.request.body;  
-    console.log("params为：", params);
-    console.log("请求地址为",`${config.host}${tarHost}${search}`)
-    const data = await axios.post(`${config.host}${tarHost}${search}`,params);
-    console.log("获取的数据为：", data.data);
-    console.log("Time:", new Date());
+    console.log("参数为：", params);
 
+    let data;
+    if(search!=null) {
+        console.log("请求地址为",`${config.host}${tarHost}${search}`);
+        data = await axios.post(`${config.host}${tarHost}${search}`,params);
+    }else {
+        console.log("请求地址为",`${config.host}${tarHost}`);
+        data = await axios.post(`${config.host}${tarHost}`,params);
+    }
+    console.log("获取的数据为：", data.data);
     ctx.body = data.data;
 })
 
